309c184ef66cfca3f452ea0e0383fa477d2bcdea
Mia <tamiapaschall@gmail.com>
2020-04-07 05:15:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,19 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    binding.pry
+    index_page = Nokogiri::HTML(open(index_url))
+    student = []
+    
+    
+    
+    
+    
+    
     
   end
 
